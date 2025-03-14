8004af8cb4d186d324ad7b130da5bb18de2b3a33
unknown <maowyt@gmail.com>
2025-03-14 08:01:45 +0200

    Implemented invalid model binding attributes and interface

--- Models/DTOs/Tasks/CreateTaskDTO.cs
+++ Models/DTOs/Tasks/CreateTaskDTO.cs
@@ -1,3 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskManagementWebAPI.Models.DTOs.Tasks;
 
-public record class CreateTaskDTO(string Title, string Description, int OwnerId);
+public record class CreateTaskDTO(
+    [Required] string Title,
+    [Required] string Description,
+    [Required, Range(1, int.MaxValue, ErrorMessage = "OwnerId must be greater than 0.")] int OwnerId
+//Using the `Range` attribute with ints to make sure if the value is missing and automatically set to the default value of 0 for ints
+//it will throw an error thanks to [ApiController] on our controllers
+);
--- Models/DTOs/Tasks/UpdateTaskDTO.cs
+++ Models/DTOs/Tasks/UpdateTaskDTO.cs
@@ -1,3 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskManagementWebAPI.Models.DTOs.Tasks;
 
-public record class UpdateTaskDTO(string? NewTitle, string? NewDescription, bool? Completed);
+public record class UpdateTaskDTO(string? NewTitle, string? NewDescription, bool? Completed) : IValidatableObject
+{
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (NewTitle == null && NewDescription == null && Completed is null)
+            yield return new ValidationResult("At least one field must be updated",
+                [nameof(NewTitle), nameof(NewDescription), nameof(Completed)]);
+    }
+}
