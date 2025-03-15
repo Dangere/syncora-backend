c47951a6ce516b6a17f8b86221bc33d3273de57e
unknown <maowyt@gmail.com>
2025-03-15 06:42:13 +0200

    Configered and implemented JWT for access token authentication and authorization

--- Program.cs
+++ Program.cs
@@ -1,26 +1,58 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using TaskManagementWebAPI.Data;
 using TaskManagementWebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Register AutoMapper
+// Register AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
 
-//Add controllers, Lifecycle: Transient but behaves like scoped because it gets created per HTTP request
+// Add controllers, Lifecycle: Transient but behaves like scoped because it gets created per HTTP request
 builder.Services.AddControllers();
 
-//Add TaskService, Lifecycle: Scoped
+// Add services, Lifecycle: Scoped
 builder.Services.AddScoped<TaskService>();
+builder.Services.AddScoped<TokenService>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<UserService>();
 
-//Add DbContext to the services, Lifecycle: Scoped
+// Add DbContext to the services, Lifecycle: Scoped
 builder.Services.AddDbContext<SyncoraDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(jwtOptions =>
+{
+    var jwtConfig = builder.Configuration.GetSection("Jwt");
+    SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(jwtConfig["SecretKey"]!));
+
+    jwtOptions.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,  // Ensure the token is from a trusted issuer
+        ValidateAudience = true,  // Ensure the token is intended for the correct audience
+        ValidateLifetime = true,  // Ensure the token hasn't expired
+        ValidateIssuerSigningKey = true,  // Validate the signature to ensure integrity
+
+        ValidIssuer = jwtConfig["Issuer"],  // Expected issuer
+        ValidAudience = jwtConfig["Audience"],  // Expected audience
+        IssuerSigningKey = key  // Use the stored secret key
+    };
+
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 app.UseRouting();
 app.MapControllers();
+
+// Using authentication and authorization middleware to secure endpoints
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapGet("/", () => "Hello World!");
 app.Run();
