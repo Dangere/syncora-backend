8c1d680a7260ac7eb7f70e47f037e611bbe8e439
unknown <maowyt@gmail.com>
2025-03-18 07:10:16 +0200

    Updated the result helper class to include optional status codes

--- Utilities/Result.cs
+++ Utilities/Result.cs
@@ -5,19 +5,20 @@ namespace LibraryManagementSystem.Utilities;
 //T is the type of the value that is returned
 //When theres an error T would be returned null if the data type is a reference type
 //When theres an error T would be returned default if the data type is a value type
-public class Result<T>(T? data, string? errorMessage = null)
+public class Result<T>(T? data, string? errorMessage = null, int? statusCode = null)
 {
     public T? Data = data;
     public bool IsSuccess => ErrorMessage == null && Data != null;
     public string? ErrorMessage => errorMessage;
+    public int? StatusCode => statusCode;
 
-    public static Result<T> Success(T data)
+    public static Result<T> Success(T data, int? statusCode = null)
     {
-        return new Result<T>(data);
+        return new Result<T>(data, null, statusCode);
     }
 
-    public static Result<T> Error(string errorMessage)
+    public static Result<T> Error(string errorMessage, int? statusCode = null)
     {
-        return new Result<T>(default, errorMessage);
+        return new Result<T>(default, errorMessage, statusCode);
     }
 }
